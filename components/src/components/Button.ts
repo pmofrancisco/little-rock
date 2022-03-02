595e4751ddb512c1b30e2497169f6fe290cd0d33
Paul Michael Francisco <pmofrancisco@yahoo.com>
2022-03-02 11:26:30 +0800

    Created components library

---
+++ components/src/components/Button.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Button = styled.button`
+  background-color: #00ce89;
+  border: none;
+  border-radius: 30px;
+  box-shadow: 0 3px 6px 0 rgba(0, 0, 0, 0.16);
+  color: #fff;
+  cursor: pointer;
+  font-size: 16px;
+  font-weight: 500;
+  padding: 18px;
+`;
